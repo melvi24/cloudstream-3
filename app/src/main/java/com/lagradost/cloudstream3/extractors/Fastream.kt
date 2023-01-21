5cf7cdaea7a0104828edf238c56f1b7d2c3f3557
Stormunblessed <stormunblessed@gmail.com>
2023-01-20 23:42:21 -0600

    extractors fix and new

--- app/src/main/java/com/lagradost/cloudstream3/extractors/Fastream.kt
+++ app/src/main/java/com/lagradost/cloudstream3/extractors/Fastream.kt
@@ -1,33 +1,31 @@
 package com.lagradost.cloudstream3.extractors
 
 import com.lagradost.cloudstream3.amap
+import com.lagradost.cloudstream3.apmap
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.M3u8Helper.Companion.generateM3u8
+import com.lagradost.cloudstream3.utils.getAndUnpack
 
-open class Fastream: ExtractorApi() {
+class Fastream: ExtractorApi() {
     override var mainUrl = "https://fastream.to"
     override var name = "Fastream"
     override val requiresReferer = false
 
 
-    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
-        val id = Regex("emb\\.html\\?(.*)\\=(enc|)").find(url)?.destructured?.component1() ?: return emptyList()
+    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink> {
         val sources = mutableListOf<ExtractorLink>()
-        val response = app.post("$mainUrl/dl",
-        data = mapOf(
-            Pair("op","embed"),
-            Pair("file_code",id),
-            Pair("auto","1")
-        )).document
+        val response = app.get(url, referer = url).document
         response.select("script").amap { script ->
-            if (script.data().contains("sources")) {
-                val m3u8regex = Regex("((https:|http:)\\/\\/.*\\.m3u8)")
-                val m3u8 = m3u8regex.find(script.data())?.value ?: return@amap
+            if (script.data().contains(Regex("eval\\(function\\(p,a,c,k,e,[rd]"))) {
+                val unpacked = getAndUnpack(script.data())
+                //val m3u8regex = Regex("((https:|http:)\\/\\/.*\\.m3u8)")
+                val newm3u8link = unpacked.substringAfter("file:\"").substringBefore("\"")
+                //val m3u8link = m3u8regex.find(unpacked)?.value ?: return@forEach
                 generateM3u8(
                     name,
-                    m3u8,
+                    newm3u8link,
                     mainUrl
                 ).forEach { link ->
                     sources.add(link)
